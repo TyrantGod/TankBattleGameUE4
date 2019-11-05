9a49b77e828f18bb1d1de7901da1a988746d7f84
tyrantgodstudios <tyrantgodstudios@gmail.com>
2019-11-04 21:52:11 -0700

    LE_16 Hit Location Setup

--- TankBattle/Source/TankBattle/TankPlayerController.cpp
+++ TankBattle/Source/TankBattle/TankPlayerController.cpp
@@ -3,6 +3,7 @@
 
 #include "TankPlayerController.h"
 
+
 void ATankPlayerController::BeginPlay()
 {
 	Super::BeginPlay();
@@ -16,10 +17,38 @@ void ATankPlayerController::BeginPlay()
 	{
 		UE_LOG(LogTemp, Warning, TEXT("Player Controller possessing: %s"), *ControlledTank->GetName());
 	}
-	UE_LOG(LogTemp, Warning, TEXT("Player Controller Begin Play"));
+}
+
+void ATankPlayerController::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+	AimToCrosshair();
 }
 
 ATank* ATankPlayerController::GetControlledTank() const
 {
 	return Cast<ATank>(GetPawn());
+}
+
+void ATankPlayerController::AimToCrosshair()
+{
+	if (!GetControlledTank())
+	{
+		return;
+	}
+
+	FVector HitLocation; //out parameter
+	if (GetSightRayHitLocation(HitLocation)) //Has effect that it ray traces
+	{
+		UE_LOG(LogTemp, Warning, TEXT("HitLocation: %s"), *HitLocation.ToString());
+	}
+	//if it hits landscape
+		//TODO tell tank to aim at this point
+}
+
+//Get world location of linetrace through crosshair, true if hit lands
+bool ATankPlayerController::GetSightRayHitLocation(FVector & OutHitLocation) const
+{
+	OutHitLocation = FVector(1.0);
+	return true;
 }
--- TankBattle/Source/TankBattle/TankPlayerController.h
+++ TankBattle/Source/TankBattle/TankPlayerController.h
@@ -21,4 +21,13 @@ public:
 	ATank* GetControlledTank() const;
 
 	virtual void BeginPlay() override;
+
+	virtual void Tick(float DeltaTime) override;
+
+	// Start moving tank barrel so for shot hit intersects world
+	void AimToCrosshair();
+
+private:
+	//return OUT para, true if it
+	bool GetSightRayHitLocation(FVector& OutHitLocation) const;
 };
